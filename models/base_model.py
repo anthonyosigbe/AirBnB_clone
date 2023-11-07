48828fa6de7aff594761c6e15e6bc41b2e840de2
Anthony Osigbe <thronesofglory@gmail.com>
2023-11-07 02:22:34 -0800

    4. Create BaseModel from dictionary tasks completed

--- models/base_model.py
+++ models/base_model.py
@@ -8,15 +8,23 @@ from datetime import datetime
 class BaseModel:
     """This signifies the foundational BaseModel of the HBnB project."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Base Model init Method
 
         Within this section, the initial values for a,
         Base Model instance are set as defaults.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        if kwargs:
+            for arg, val in kwargs.items():
+                if arg in ('created_at', 'updated_at'):
+                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+
+                if arg != '__class__':
+                    setattr(self, arg, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
 
     def __str__(self):
         """Provide the printed or string representation,
