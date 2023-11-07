4cbcd6c76f9142840e3b15881203efbe4d47472f
braveking12 <davidkadofficial@gmail.com>
2023-11-07 16:57:06 +0000

    6. Console 0.0.1 basic control model

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+"""cmd control module console.py, for test specific functionality"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    # Initialize the prompt string
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        return True
+
+    def help_quit(self, arg):
+        print('quit command: Exit the program')
+
+    def do_EOF(self, arg):
+        print()  # Print a new line for better formatting
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
