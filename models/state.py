975c59d1365ee4afdbf50867823bf521dbb2095f
Anthony Osigbe <thronesofglory@gmail.com>
2023-11-08 09:39:55 -0800

    9. More classes! tasks completed

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Depicts the State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represent a state.
+
+    Att:
+        name (str): The name of the state.
+    """
+
+    name = ""
