0a07812b90ac940df0abeb511936e4c0411779f9
braveking12 <davidkadofficial@gmail.com>
2023-11-11 03:47:55 +0000

    update to user

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """Represent a User.
 
-    Attributes:
+    Attr:
         All user information collected in string format
     """
 
